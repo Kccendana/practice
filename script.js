48b71be14ba0b2a432ecbd06cc20295f95dc8755
kcccendana <kc_telyn@yahoo.com>
2023-06-20 15:10:30 +0900

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+/* eslint linebreak-style: ["error", "windows"]  */
+const navButton = document.querySelector('.nav-button');
+const navList = document.querySelector('.desktop-view-links');
+const navMenu = document.querySelectorAll('.navMenu');
+const logo = document.querySelector('.logo');
+
+navButton.addEventListener('click', () => {
+  navList.classList.toggle('show');
+  navButton.classList.toggle('close');
+  logo.classList.toggle('hide');
+}, false);
+
+navMenu.forEach((links) => links.addEventListener('click', () => {
+  navList.classList.remove('show');
+  navButton.classList.remove('close');
+  logo.classList.remove('hide');
+}));
+
+window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); };
